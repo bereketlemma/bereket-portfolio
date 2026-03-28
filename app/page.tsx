c8cee901112a55c56b6083218e16a2d42087a329
bereket <blemma25@my.whitworth.edu>
2026-03-28 15:00:51 -0700

    feat: wire all home sections to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Hero from "./home/_components/hero"
+import Experience from "./home/_components/experience"
+import Projects from "./home/_components/projects"
+import Skills from "./home/_components/skills"
+import Competitive from "./home/_components/competitive"
+import Footer from "./home/_components/footer"
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Experience />
+      <Projects />
+      <Skills />
+      <Competitive />
+      <Footer />
     </>
   )
 }

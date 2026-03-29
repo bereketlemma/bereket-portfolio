004649bc45f794c34c3c52280be3d39bd29bb22e
bereket <blemma25@my.whitworth.edu>
2026-03-28 17:23:05 -0700

    fix: remove competitive section from home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "./home/_components/hero"
 import Experience from "./home/_components/experience"
 import Projects from "./home/_components/projects"
 import Skills from "./home/_components/skills"
-import Competitive from "./home/_components/competitive"
 import Footer from "./home/_components/footer"
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <Experience />
       <Projects />
       <Skills />
-      <Competitive />
       <Footer />
     </>
   )

e48200d64c75da99bbbb2656519571469b9d513a
bereket <blemma25@my.whitworth.edu>
2026-03-29 01:01:10 -0700

    Delete app/home/page.tsx

--- app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col">
-      <p className="text-accent font-mono">Portfolio coming soon...</p>
-    </main>
-  )
-}

c4fd27919318752304c0b63463462d8ae78b9b35
bereket <blemma25@my.whitworth.edu>
2026-03-29 22:21:24 -0700

    fix: hobbies grid 2x2 layout

--- app/home/_components/hobbies.tsx
+++ app/home/_components/hobbies.tsx
@@ -123,7 +123,7 @@ export default function Hobbies() {
       </div>
 
       {/* Grid */}
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         {hobbies.map((hobby, i) => (
           <HobbyCard key={i} hobby={hobby} index={i} />
         ))}
